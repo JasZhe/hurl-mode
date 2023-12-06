29efae8bdb32d305a8e2f3baa1152015bff11800
Jason Zhen <15258281+JasZhe@users.noreply.github.com>
2023-12-06 02:14:14 -0500

    cleanup file headers and other lints
    - root group should not have "mode" in it

--- hurl-mode.el
+++ hurl-mode.el
@@ -1,50 +1,80 @@
-;;; hurl-mode.el --- major mode for hurl
+;;; hurl-mode.el --- Major mode for hurl
+;;
+;; Copyright (C) 2023 Jason Zhen
+;;
+;; Author: Jason Zhen <jaszhe@gmail.com>
+;; Maintainer: Jason Zhen <jaszhe@gmail.com>
+;; Created: November 17, 2023
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "28.2"))
+;; URL: https://github.com/JasZhe/hurl-mode
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+;;
 ;;; Commentary:
-;;; simple major mode for editing hurl files
+;; simple major mode for editing hurl files
 
 ;;; Code:
+(require 'org-src)
 
 (eval-when-compile
   (require 'rx))
 
-(defgroup hurl-mode nil
+(defgroup hurl nil
   "A major mode for editing Hurl files."
   :group 'tools)
 
-(defgroup hurl-mode-faces nil
+(defgroup hurl-faces nil
   "Faces used in Hurl Mode"
-  :group 'hurl-mode
+  :group 'hurl
   :group 'faces)
 
 (defface hurl-mode-method-face 
   '((t (:inherit font-lock-keyword-face)))
   "Face for HTTP method."
-  :group 'hurl-mode-faces)
+  :group 'hurl-faces)
 
 (defface hurl-mode-url-face
   '((t (:inherit font-lock-function-name-face)))
   "Face for urls"
-  :group 'hurl-mode-faces)
+  :group 'hurl-faces)
 
 (defface hurl-mode-section-face
   '((t (:inherit font-lock-builtin-face)))
   "Face for hurl sections"
-  :group 'hurl-mode-faces)
+  :group 'hurl-faces)
 
 (defface hurl-mode-header-names-face
   '((t (:inherit font-lock-variable-name-face)))
   "Face for header names"
-  :group 'hurl-mode-faces)
+  :group 'hurl-faces)
 
 (defface hurl-mode-header-value-face
   '((t (:inherit font-lock-string-face)))
   "Face for header values"
-  :group 'hurl-mode-faces)
+  :group 'hurl-faces)
 
 (defface hurl-mode-body-face
   '((t (:inherit font-lock-constant-face)))
   "Face for body values"
-  :group 'hurl-mode-faces)
+  :group 'hurl-faces)
 
 
 (defconst hurl-mode--http-method-keywords
