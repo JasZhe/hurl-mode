960bf44d45055df1e3ffd7aa6896aeb83b716fb6
Jason Zhen <15258281+JasZhe@users.noreply.github.com>
2023-12-07 01:28:40 -0500

    add font lock regexps for asserts/captures

--- hurl-mode.el
+++ hurl-mode.el
@@ -57,7 +57,7 @@
   :group 'hurl-faces)
 
 (defface hurl-mode-section-face
-  '((t (:inherit font-lock-builtin-face)))
+  '((t (:inherit font-lock-preprocessor-face)))
   "Face for hurl sections"
   :group 'hurl-faces)
 
@@ -71,10 +71,52 @@
   "Face for header values"
   :group 'hurl-faces)
 
+(defface hurl-mode-variable-face
+  '((t (:inherit font-lock-variable-name-face)))
+  "Face for hurl variables"
+  :group 'hurl-faces)
+
+(defface hurl-mode-variable-value-face
+  '((t (:inherit font-lock-string-face)))
+  "Face for hurl variable values"
+  :group 'hurl-faces)
+
+(defface hurl-mode-query-face
+  '((t (:inherit font-lock-builtin-face)))
+  "Face for hurl query keywords"
+  :group 'hurl-faces)
+
+(defface hurl-mode-query-arg-face
+  '((t (:inherit font-lock-string-face)))
+  "Face for hurl query arguments"
+  :group 'hurl-faces)
+
+(defface hurl-mode-filter-face
+  '((t (:inherit font-lock-builtin-face)))
+  "Face for hurl filters"
+  :group 'hurl-faces)
+
+(defface hurl-mode-filter-arg-face
+  '((t (:inherit font-lock-string-face)))
+  "Face for hurl filter arguments"
+  :group 'hurl-faces)
+
+(defface hurl-mode-pred-face
+  '((t (:inherit font-lock-operator-face)))
+  "Face for hurl filter arguments"
+  :group 'hurl-faces)
+
+(defface hurl-mode-pred-arg-face
+  '((t (:inherit font-lock-string-face)))
+  "Face for hurl filter arguments"
+  :group 'hurl-faces)
+
 (defface hurl-mode-body-face
-  '((t (:inherit font-lock-constant-face)))
+  '((t (:inherit font-lock-string-face)))
   "Face for body values"
   :group 'hurl-faces)
+
+
 
 
 (defconst hurl-mode--http-method-keywords
@@ -89,8 +131,7 @@
   '("[QueryStringParams]" "[FormParams]" "[MultipartFormData]" "[BasicAuth]" "[Cookies]" "[Asserts]" "[Captures]" "[Options]"))
 (defconst hurl-mode--section-header-regexp
   (rx-to-string `(: bol
-                  (or ,@hurl-mode--section-header-keywords))))
-
+                    (or ,@hurl-mode--section-header-keywords))))
 
 (defconst hurl-mode--expected-response-regexp
   (rx-to-string `(: bol
@@ -103,6 +144,79 @@
                   (group (* (or alpha "_" "-")) ":")
                   (+ blank)
                   (group (* any)))))
+
+(defconst hurl-mode--arg-queries
+  '("header" "certificate" "cookie" "xpath" "jsonpath" "regex" "variable"))
+
+(defconst hurl-mode--no-arg-queries
+  '("status" "url" "body" "duration" "sha256" "md5" "bytes"))
+
+(defconst hurl-mode--certificate-attrs
+  `("Subject" "Issuer" "Start-Date" "Expire-Date" "Serial-Number"))
+
+
+(defconst hurl-mode--predicates
+  '("==" "!=" ">" ">=" "<" "<=" "startsWith" "endsWith" "contains" "includes" "matches" "exists" "isBoolean" "isCollection" "isDate" "isEmpty" "isFloat" "isInteger" "isString"))
+
+(defconst hurl-mode--no-arg-filters
+  '("count" "daysAfterNow" "daysBeforeNow" "decode" "format" "htmlEscape" "htmlUnescape" "toDate" "toInt" "urlDecode" "urlEncode"))
+
+(defconst hurl-mode--single-int-arg-filters '("nth"))
+(defconst hurl-mode--single-string-arg-filters '("regex" "split" "xpath"))
+(defconst hurl-mode--double-string-arg-filters `("replace"))
+
+(defconst hurl-mode--capture-regexp
+  (rx-to-string `(: bol
+		    ;; variable name
+		    (group (* (or alpha "_" "-")) ":")
+		    (* blank)
+		    ;; query
+		    (group (or (or ,@hurl-mode--no-arg-queries)
+			       (: (or ,@hurl-mode--arg-queries))
+			       ))
+		    (* blank)
+		    ;; optional query param
+		    (? (group (or (: "\"" (* (or (not "\"") "\\\"") ) "\"")
+				  (or ,@hurl-mode--certificate-attrs))))
+		    (* blank)
+		    ;; optional filter
+		    (* (: (group (or (or ,@hurl-mode--no-arg-filters)
+				     (or ,@hurl-mode--single-int-arg-filters)
+				     (or ,@hurl-mode--single-string-arg-filters)
+				     (or ,@hurl-mode--double-string-arg-filters)))
+			  (* blank)
+			  ;; optional filter params, single or double string args, single int
+			  (? (group (* digit)))
+			  (? (group (: "\"" (* alnum) "\"")))
+			  (* blank)
+			  (? (group (: "\"" (* alnum) "\""))))))))
+
+(defconst hurl-mode--assert-regexp
+  (rx-to-string `(: bol
+		    ;; query
+		    (group (or (or ,@hurl-mode--no-arg-queries)
+			       (or ,@hurl-mode--arg-queries)))
+		    (* blank)
+		    ;; optional query arg
+		    (? (group (or (: "\"" (* (or (not "\"") "\\\"") ) "\"")
+				  ,@hurl-mode--certificate-attrs)))
+		    (* blank)
+		    ;; optional filter
+		    (* (: (group (or ,@hurl-mode--no-arg-filters
+				     ,@hurl-mode--single-int-arg-filters
+				     ,@hurl-mode--single-string-arg-filters
+				     ,@hurl-mode--double-string-arg-filters))
+			  (* blank)
+			  ;; optional filter args, single int, single or double string
+			  (? (group (* digit)))
+			  (? (group (: "\"" (* (category ascii)) "\"")))
+			  (* blank)
+			  (? (group (: "\"" (* (category ascii)) "\"")))))
+		    ;; predicate
+		    (group (or ,@hurl-mode--predicates))
+		    ;; optional predicate arg
+		    (? (group (* (category ascii))))
+		    )))
 
 
 ;; match ``` then anything except for ` then another ``` and end of line
@@ -192,9 +306,29 @@ extend the font lock region."
 
 
 (defconst hurl-mode-keywords
-  `((,hurl-mode--http-method-regexp (1 'hurl-mode-method-face) (2 'hurl-mode-url-face))
-    (,hurl-mode--expected-response-regexp (1 'hurl-mode-method-face) (2 'hurl-mode-url-face))
-    (,hurl-mode--header-regexp (1 'hurl-mode-header-names-face) (2 'hurl-mode-header-value-face))
+  `((,hurl-mode--http-method-regexp (1 'hurl-mode-method-face)
+				    (2 'hurl-mode-url-face))
+    (,hurl-mode--expected-response-regexp (1 'hurl-mode-method-face)
+					  (2 'hurl-mode-url-face))
+    (,hurl-mode--header-regexp (1 'hurl-mode-header-names-face)
+			       (2 'hurl-mode-header-value-face))
+    (,hurl-mode--assert-regexp (1 'hurl-mode-query-face)
+			       (2 'hurl-mode-query-arg-face)
+			       (3 'hurl-mode-filter-face)
+			       (4 'hurl-mode-filter-arg-face)
+			       (5 'hurl-mode-filter-arg-face)
+			       (6 'hurl-mode-filter-arg-face)
+			       (7 'hurl-mode-pred-face)
+			       (8 'hurl-mode-pred-arg-face)
+			       )
+    (,hurl-mode--capture-regexp (1  'hurl-mode-variable-face)
+				(2 'hurl-mode-query-face)
+				(3 'hurl-mode-query-arg-face)
+				(4 'hurl-mode-filter-face)
+				(5 'hurl-mode-filter-arg-face) ;; int arg
+				(6 'hurl-mode-filter-arg-face) ;; string1
+				(7 'hurl-mode-filter-arg-face) ;; string2
+				)
     (hurl-highlight-filter-xml)
     (hurl-highlight-filter-json)
     (hurl-highlight-filter-graphql)
